ca0ae05c92ee68376dd0964f28f2191db4e27073
Ваше Имя <you@example.com>
2026-07-21 13:50:10 +0300

    Улучшил код

--- backend/api/endpoints/auth.py
+++ backend/api/endpoints/auth.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
 from starlette import status
 from pwdlib import PasswordHash
 
-from backend.DB.database import connect_to_db
-from backend.core.custom_exceptions import UserAlreadyExistsError, UserNotFoundError, WrongPasswordError, \
-    RefreshTokenExpiredError, RefreshTokenNotFoundError
-from backend.repositories.jwt import JWTRepository
-from backend.repositories.user import UserRepository
+from backend.core.custom_exceptions import (
+    UserAlreadyExistsError,
+    UserNotFoundError,
+    WrongPasswordError,
+    RefreshTokenExpiredError,
+    RefreshTokenNotFoundError,
+)
 from backend.api.schemas.user import UserRegister, UserResponse
 from backend.services.user import UserService
+from dependencies.puzzles import get_user_service
 
 pwd_context = PasswordHash.recommended()
 
@@ -20,46 +23,47 @@ auth_router = APIRouter(
     tags=["auth"],
 )
 
-@auth_router.post("/register/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def register_user(user_reg: UserRegister,
-                        session: AsyncSession = Depends(connect_to_db)):
-    try:
-        repo = UserRepository(session)
-        service = UserService(repo)
-        user = await service.add_user(user_reg)#Придёт словарь с двумя ключами, нужно, чтобы вернуть us и id
-        return UserResponse(username=user["username"],id=user["id"])
 
+@auth_router.post(
+    "/register/", response_model=UserResponse, status_code=status.HTTP_201_CREATED
+)
+async def register_user(
+    user_reg: UserRegister,
+    user_service: Annotated[UserService, Depends(get_user_service)],
+) -> UserResponse:
+    try:
+        user = await user_service.add_user(user_reg)
+        return UserResponse(username=user["username"], id=user["id"])
     except UserAlreadyExistsError:
         raise HTTPException(status_code=409, detail="User already exists")
 
 
 @auth_router.post("/login")
 async def login(
-        form_data: OAuth2PasswordRequestForm = Depends(),
-        session: AsyncSession = Depends(connect_to_db)):
-    repo = UserRepository(session)
-    jwt_repo = JWTRepository(session)
-    service = UserService(repo,jwt_repo)
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    user_service: Annotated[UserService, Depends(get_user_service)],
+):
     try:
-        res = await service.login(form_data)
+        res = await user_service.login(form_data)
     except UserNotFoundError:
         raise HTTPException(status_code=404, detail="User not found")
     except WrongPasswordError:
         raise HTTPException(status_code=400, detail="Wrong password")
     return res
 
+
 @auth_router.post("/refresh")
-async def get_refresh_token(refresh_token:str,
-                            session: AsyncSession = Depends(connect_to_db)):
+async def get_refresh_token(
+    refresh_token: str, user_service: Annotated[UserService, Depends(get_user_service)]
+):
     try:
-        user_repo = UserRepository(session)
-        jwt_repo = JWTRepository(session)
-        service = UserService(user_repo,jwt_repo)
-        res = await service.refresh(refresh_token)
+        res = await user_service.refresh(refresh_token)
         return res
     except UserNotFoundError:
         raise HTTPException(status_code=404, detail="User not found")
     except RefreshTokenExpiredError:
         raise HTTPException(status_code=401, detail="Refresh token expired")
     except RefreshTokenNotFoundError:
-        raise HTTPException(status_code=400, detail="Refresh token not found or smth went wrong")
+        raise HTTPException(
+            status_code=400, detail="Refresh token not found or smth went wrong"
+        )
--- backend/api/endpoints/puzzles.py
+++ backend/api/endpoints/puzzles.py
@@ -1,98 +1,94 @@
 from typing import Annotated
-
 from arq import ArqRedis
 from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.api.schemas.puzzles import Puzzle, UserSolution
-from backend.DB.database import connect_to_db
 from backend.api.schemas.puzzles import PuzzleTestPD, PuzzleResponse
 from backend.api.dependencies.auth import get_current_user
-from backend.repositories.puzzles import PuzzlesRepository
-from backend.core.custom_exceptions import PuzzleNotFoundError, TestNotFoundError, SubmissionNotFoundError
+from backend.core.custom_exceptions import (
+    PuzzleNotFoundError,
+    TestNotFoundError,
+    SubmissionNotFoundError,
+)
 from backend.services.puzzle import PuzzleService
-from backend.repositories.puzzle_test import PuzzleTestRepository
 from backend.api.schemas.puzzles import PuzzleTestPDResponse
-from backend.api.dependencies.puzzles import get_redis_pool
-from backend.repositories.submission import SubmissionRepository
+from backend.api.dependencies.puzzles import (
+    get_redis_pool,
+    get_puzzle_service,
+    get_submission_service,
+)
 from backend.services.submission import SubmissionService
 
-puzzle_router = APIRouter(
-    prefix="/puzzles",
-    tags=["Puzzle"]
-)
+puzzle_router = APIRouter(prefix="/puzzles", tags=["Puzzle"])
 
 
-@puzzle_router.get("/{puzzle_id}",response_model=PuzzleResponse)
-async def get_puzzle(puzzle_id: int,
-                     session: Annotated[AsyncSession, Depends(connect_to_db)]):
+@puzzle_router.get("/{puzzle_id}", response_model=PuzzleResponse)
+async def get_puzzle(
+    puzzle_id: int,
+    puzzle_service: Annotated[PuzzleService, Depends(get_puzzle_service)],
+):
     try:
-        puzzles_repo = PuzzlesRepository(session)
-        puzzle_service = PuzzleService(puzzle_repo=puzzles_repo)
         puzzle = await puzzle_service.get_puzzle(puzzle_id)
         return puzzle
     except PuzzleNotFoundError:
         raise HTTPException(status_code=404, detail="Puzzle not found")
 
 
-@puzzle_router.post("/create_puzzle",response_model=PuzzleResponse)
-async def create_puzzle(puzzle: Puzzle,
-                        session: Annotated[AsyncSession, Depends(connect_to_db)]):
-    puzzle_repo = PuzzlesRepository(session)
-    puzzle_service = PuzzleService(puzzle_repo)
+@puzzle_router.post("/create_puzzle", response_model=PuzzleResponse)
+async def create_puzzle(
+    puzzle: Puzzle,
+    puzzle_service: Annotated[PuzzleService, Depends(get_puzzle_service)],
+):
     new_puzzle = await puzzle_service.create_puzzle(puzzle)
     return new_puzzle
 
 
-@puzzle_router.post("/create_test/{puzzle_id}",response_model=PuzzleTestPDResponse)
-async def create_test(puzzle_id:int,
-                test: PuzzleTestPD,
-                session:Annotated[AsyncSession, Depends(connect_to_db)]):
+@puzzle_router.post("/create_test/{puzzle_id}", response_model=PuzzleTestPDResponse)
+async def create_test(
+    puzzle_id: int,
+    test: PuzzleTestPD,
+    puzzle_service: Annotated[PuzzleService, Depends(get_puzzle_service)],
+):
     try:
-        puzzle_repo = PuzzlesRepository(session)
-        puzzle_test_repo = PuzzleTestRepository(session)
-        puzzle_service = PuzzleService(puzzle_repo=puzzle_repo, puzzle_test_repo=puzzle_test_repo)
         new_test = await puzzle_service.create_test(puzzle_id, test)
         return new_test
     except PuzzleNotFoundError:
         raise HTTPException(status_code=404, detail="Puzzle not found")
 
 
-@puzzle_router.post("/check_solution")
-async def check_user_solution(user_sol: UserSolution,
-                        puzzle_id: int,
-                        arq_pool: Annotated[ArqRedis, Depends(get_redis_pool)],
-                        current_user: Annotated[str, Depends(get_current_user)],
-                        session: Annotated[AsyncSession, Depends(connect_to_db)]
-                        ):
-        puzzle_repo = PuzzlesRepository(session)
-        submission_repo = SubmissionRepository(session)
-        puzzle_service = PuzzleService(puzzle_repo=puzzle_repo, submission_repo=submission_repo)
-        try:
-            return await puzzle_service.send_user_solution(puzzle_id, user_sol,arq_pool)
-        except PuzzleNotFoundError:
-            raise HTTPException(status_code=404, detail="Puzzle not found")
+@puzzle_router.post("/check_solution/{puzzle_id}")
+async def check_user_solution(
+    user_sol: UserSolution,
+    puzzle_id: int,
+    arq_pool: Annotated[ArqRedis, Depends(get_redis_pool)],
+    current_user: Annotated[str, Depends(get_current_user)],
+    puzzle_service: Annotated[PuzzleService, Depends(get_puzzle_service)],
+):
+    try:
+        return await puzzle_service.send_user_solution(puzzle_id, user_sol, arq_pool)
+    except PuzzleNotFoundError:
+        raise HTTPException(status_code=404, detail="Puzzle not found")
 
 
 @puzzle_router.delete("/delete/{puzzle_id}")
-async def delete_puzzle(puzzle_id: int,
-                        session: Annotated[AsyncSession, Depends(connect_to_db)]):
-    puzzle_repo = PuzzlesRepository(session)
-    puzzle_service = PuzzleService(puzzle_repo=puzzle_repo)
+async def delete_puzzle(
+    puzzle_id: int,
+    puzzle_service: Annotated[PuzzleService, Depends(get_puzzle_service)],
+):
     try:
         del_puzzle = await puzzle_service.delete_puzzle(puzzle_id)
-        return {"message": "Puzzle deleted","info":del_puzzle.id}
+        return {"message": "Puzzle deleted", "info": del_puzzle.id}
     except PuzzleNotFoundError:
-        raise HTTPException(status_code=404, detail="Puzzle not found. Nothing to delete")
-
+        raise HTTPException(
+            status_code=404, detail="Puzzle not found. Nothing to delete"
+        )
 
 
 @puzzle_router.get("/get_test/{test_id}")
-async def get_test(test_id: int,
-                   session: Annotated[AsyncSession, Depends(connect_to_db)]):
-    puzzle_test_repo = PuzzleTestRepository(session)
-    puzzle_service = PuzzleService(puzzle_test_repo=puzzle_test_repo)
+async def get_test(
+    test_id: int, puzzle_service: Annotated[PuzzleService, Depends(get_puzzle_service)]
+):
     try:
         test = await puzzle_service.get_test(test_id)
         return test
@@ -101,23 +97,23 @@ async def get_test(test_id: int,
 
 
 @puzzle_router.delete("/test/delete/{test_id}")
-async def delete_test(test_id: int,
-                      session: Annotated[AsyncSession, Depends(connect_to_db)]):
+async def delete_test(
+    test_id: int, puzzle_service: Annotated[PuzzleService, Depends(get_puzzle_service)]
+):
     try:
-        puzzle_test_repo = PuzzleTestRepository(session)
-        puzzle_service = PuzzleService(puzzle_test_repo=puzzle_test_repo)
         await puzzle_service.delete_test(test_id)
         return {"message": "Test deleted"}
     except TestNotFoundError:
-        raise HTTPException(status_code=404, detail="Test not found.Nothing to be deleted")
-
+        raise HTTPException(
+            status_code=404, detail="Test not found.Nothing to be deleted"
+        )
 
 
 @puzzle_router.get("/get_submission/{sub_id}")
-async def get_submission(sub_id: int,
-                         session: Annotated[AsyncSession, Depends(connect_to_db)]):
-    submission_repo = SubmissionRepository(session)
-    submission_service = SubmissionService(submission_repo=submission_repo)
+async def get_submission(
+    sub_id: int,
+    submission_service: Annotated[SubmissionService, Depends(get_submission_service)],
+):
     try:
         submission = await submission_service.get_submission(sub_id)
         return submission
